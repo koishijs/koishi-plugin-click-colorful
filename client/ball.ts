d75bdcddf2c32ec0d81f101f93eabfe44ea50181
Shigma <shigma10826@gmail.com>
2023-07-19 22:08:40 +0800

    feat: support client config

--- client/ball.ts
+++ client/ball.ts
@@ -1,102 +1,97 @@
-const defaultParams = {
-  colors: ['#eb125f', '#6eff8a', '#6386ff', '#f9f383'],
-  size: 30,
-  maxCount: 30
+export interface ClickOptions {
+  enabled: boolean
+  size: number
+  count: number
+  colors: string[]
 }
-function Ball(params?: any) {
-  this.params = Object.assign({}, defaultParams, params)
+
+function randomPick<T>(arr: T[]) {
+  return arr[Math.floor(Math.random() * arr.length)]
 }
-function getOneRandom(arr)
-{
-  return arr[Math.floor(Math.random()*arr.length)];
+
+class Ball {
+  constructor(public options: ClickOptions) {}
+
+  fly(x: number, y: number, loopTimer = 300) {
+    const ballElements: HTMLElement[] = []
+    const fragment = document.createDocumentFragment()
+
+    let loop = 0
+    for (let i = 0; i < this.options.count; i++) {
+      const curLoop = i / this.options.count
+      const ball = document.createElement('i')
+      ball.className = 'color-ball ball-loop-' + curLoop
+      let blurX = Math.random() * 10
+      if (Math.random() > 0.5) blurX = blurX * -1
+      let blurY = Math.random() * 10
+      if (Math.random() > 0.5) blurY = blurY * -1
+      ball.style.left = (x - this.options.size) + 'px'
+      ball.style.top = (y - this.options.size) + 'px'
+      ball.style.width = (2 * this.options.size) + 'px'
+      ball.style.height = (2 * this.options.size) + 'px'
+      ball.style.position = 'fixed'
+      ball.style.borderRadius = '1000px'
+      ball.style.boxSizing = 'border-box'
+      ball.style.zIndex = '9999'
+      ball.style.opacity = '0'
+      if (curLoop === 0) ball.style.opacity = '1'
+      ball.style.transform = 'translate3d(0px, 0px, 0px) scale(1)'
+      ball.style.webkitTransform = 'translate3d(0px, 0px, 0px) scale(1)'
+      ball.style.transition = 'transform 1s ' + curLoop * loopTimer / 1000 + 's ease-out'
+      ball.style.webkitTransition = 'transform 1s ' + curLoop * loopTimer / 1000 + 's ease-out'
+      ball.style.backgroundColor = randomPick(this.options.colors)
+      fragment.appendChild(ball)
+      ballElements.push(ball)
+      setTimeout(function () {}, curLoop * loopTimer + 30)
+      if (curLoop !== loop) {
+        (function (num) {
+          setTimeout(function () {
+            const loopBalls = document.getElementsByClassName('ball-loop-' + num)
+            for (let j = 0; j < loopBalls.length; j++) {
+              loopBalls[j].style.opacity = 1
+            }
+            if (num === loop) {
+              _clear(ballElements)
+            }
+          }, num * loopTimer + 30)
+        })(curLoop)
+        loop = curLoop
+      }
+    }
+
+    document.body.appendChild(fragment)
+    // 延迟删除
+    _clear(ballElements)
+    // 执行动画
+    setTimeout(function () {
+      for (let i = 0; i < ballElements.length; i++) {
+        _run(ballElements[i])
+      }
+    }, 10)
+  }
 }
-function _run(ball) {
-  let randomXFlag = Math.random() > 0.5
-  let randomYFlag = Math.random() > 0.5
-  let randomX = Math.random() * 160;
-  let randomY = Math.random() * 160;
+
+function _run(ball: HTMLElement) {
+  const randomXFlag = Math.random() > 0.5
+  const randomYFlag = Math.random() > 0.5
+  let randomX = Math.random() * 160
+  let randomY = Math.random() * 160
   if (randomXFlag) {
-      randomX = randomX * -1;
+    randomX = randomX * -1
   }
   if (randomYFlag) {
-      randomY = randomY * -1
+    randomY = randomY * -1
   }
-  let transform = 'translate3d('+randomX+'px,' + randomY + 'px, 0) scale(0)';
-  ball.style.webkitTransform = transform;
-  ball.style.MozTransform = transform;
-  ball.style.msTransform = transform;
-  ball.style.OTransform = transform;
-  ball.style.transform = transform;
+  const transform = 'translate3d(' + randomX + 'px,' + randomY + 'px, 0) scale(0)'
+  ball.style.transform = transform
 }
-Ball.prototype.fly = function (x, y, playCount, loopTimer) {
-  if (!loopTimer) loopTimer = 300
-  let ballElements = []
-  let fragment = document.createDocumentFragment()
-  
-  let ballNum = this.params.maxCount;
-  // 修改轮换播放实现方式，改为一次创建所有，通过延迟执行动画实现
-  if(playCount) {
-    ballNum = ballNum * playCount;
-  } 
-  let loop = 0
-  for(let i=0; i<ballNum; i++) {
-    let curLoop = i/this.params.maxCount
-    let ball = document.createElement('i');
-    ball.className = 'color-ball ball-loop-' + curLoop;
-    let blurX = Math.random() * 10
-    if (Math.random() > 0.5) blurX = blurX* -1
-    let blurY = Math.random() * 10
-    if (Math.random() > 0.5) blurY = blurY* -1
-    ball.style.left = (x) + 'px';
-    ball.style.top = (y) + 'px';
-    ball.style.width = this.params.size + 'px';
-    ball.style.height = this.params.size + 'px';
-    ball.style.position = 'fixed';
-    ball.style.borderRadius = '1000px';
-    ball.style.boxSizing = 'border-box';
-    ball.style.zIndex = '9999';
-    ball.style.opacity = '0';
-    if (curLoop === 0) ball.style.opacity = '1';
-    ball.style.transform = 'translate3d(0px, 0px, 0px) scale(1)';
-    ball.style.webkitTransform = 'translate3d(0px, 0px, 0px) scale(1)';
-    ball.style.transition = 'transform 1s ' + curLoop * loopTimer / 1000 + 's ease-out';
-    ball.style.webkitTransition = 'transform 1s ' + curLoop * loopTimer / 1000 + 's ease-out';
-    ball.style.backgroundColor = getOneRandom(this.params.colors);
-    fragment.appendChild(ball);
-    ballElements.push(ball)
-    // 性能优化终极版
-    if (curLoop !== loop) {
-      (function(num){
-        setTimeout(function(){
-          let loopBalls = document.getElementsByClassName('ball-loop-' + num)
-          for(let j = 0; j < loopBalls.length; j++) {
-            loopBalls[j].style.opacity = 1
-          }
-          if (num === loop) {
-            _clear(ballElements)
-          }
-        }, num * loopTimer + 30)
-      })(curLoop)
-      loop = curLoop
-    }
-  }
-  
-  document.body.appendChild(fragment);
-  // 延迟删除
-  !playCount &&  _clear(ballElements)
-  // 执行动画
+
+function _clear(balls: HTMLElement[]) {
   setTimeout(function () {
-    for(let i=0; i<ballElements.length; i++){
-      _run(ballElements[i])
-    }	
-  }, 10)
+    for (const ball of balls) {
+      ball.parentElement?.removeChild(ball)
+    }
+  }, 1000)
 }
-function _clear(balls) {
-  setTimeout(function() {
-    for(let i=0; i<balls.length; i++){
-      document.body.removeChild(balls[i])
-    }	
-  }, 1000 )
-  
-}
+
 export { Ball }
--- client/index.ts
+++ client/index.ts
@@ -1,21 +1,59 @@
-import { Context } from '@koishijs/client'
+import { Context, Schema, useConfig } from '@koishijs/client'
 import { Ball } from './ball'
 
+declare module '@koishijs/client' {
+  interface Config {
+    click: {
+      enabled: boolean
+      size: number
+    }
+  }
+}
+
 export default (ctx: Context) => {
-  function playColor(e: MouseEvent) {
-    const color = new Ball()
+  ctx.settings({
+    id: 'click',
+    title: '点击特效',
+    schema: Schema.object({
+      click: Schema.object({
+        enabled: Schema.boolean().description('启用点击特效。').default(true),
+        size: Schema.number().description('形状大小。').min(0).step(1).default(15),
+        count: Schema.number().description('形状数量。').min(0).max(50).step(1).default(30),
+        colors: Schema.array(Schema.string().required().role('color')).description('形状颜色。').default(['#eb125f', '#6eff8a', '#6386ff', '#f9f383']),
+      }).description('点击特效'),
+    }),
+  })
+
+  const config = useConfig()
+
+  function handleClick(e: MouseEvent) {
+    if (config.value.click?.enabled === false) return
+    const color = new Ball({
+      size: 15,
+      count: 30,
+      colors: ['#eb125f', '#6eff8a', '#6386ff', '#f9f383'],
+      ...config.value.click,
+    })
     color.fly(e.clientX, e.clientY)
   }
-  function toucuPlayColor(e: TouchEvent) {
-    const color = new Ball()
-    color.fly(e.changedTouches[0].clientX,e.changedTouches[0].clientY)
+
+  function handleTouch(e: TouchEvent) {
+    if (config.value.click?.enabled === false) return
+    const color = new Ball({
+      size: 15,
+      count: 30,
+      colors: ['#eb125f', '#6eff8a', '#6386ff', '#f9f383'],
+      ...config.value.click,
+    })
+    color.fly(e.changedTouches[0].clientX, e.changedTouches[0].clientY)
     e.stopPropagation()
     e.preventDefault()
   }
-  addEventListener('click', playColor)
-  addEventListener('touchstart', toucuPlayColor)
+
+  addEventListener('click', handleClick)
+  addEventListener('touchstart', handleTouch)
   ctx.on('dispose', () => {
-    removeEventListener('click', playColor)
-    removeEventListener('touchstart', toucuPlayColor)
+    removeEventListener('click', handleClick)
+    removeEventListener('touchstart', handleTouch)
   })
 }
